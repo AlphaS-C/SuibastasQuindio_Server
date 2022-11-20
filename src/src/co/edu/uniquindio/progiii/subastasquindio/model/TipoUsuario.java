7e8ccc7fc63f856f77cb166db0306565bd58b5ac
alphas-c <mainazirdsdelabeta@gmail.com>
2022-11-19 19:05:57 -0500

    Fixeado archivos duplicados

--- src/src/co/edu/uniquindio/progiii/subastasquindio/model/TipoUsuario.java
+++
@@ -1,8 +0,0 @@
-package co.edu.uniquindio.progiii.subastasquindio.model;
-
-import java.io.Serializable;
-
-public enum TipoUsuario implements Serializable {
-    COMPRADOR,
-    VENDEDOR
-}
--- src/src/co/edu/uniquindio/progiii/subastasquindio/services/IUsuario.java
+++
@@ -1,8 +0,0 @@
-package co.edu.uniquindio.progiii.subastasquindio.services;
-
-public interface IUsuario {
-
-    public void login();
-    public void logout();
-
-}
